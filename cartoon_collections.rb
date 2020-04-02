f3899e4323a50ce94e00d108213dd9e722f6e3a3
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-02 23:54:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,5 +13,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types) {|cheese| cheese}
+  array.find {|cheese| cheese_types.include?(cheese)
 end
