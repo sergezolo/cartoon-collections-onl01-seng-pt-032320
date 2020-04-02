9bf0a289493c9ffee43eb6d423e2ec8ff245ed1d
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-02 21:48:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,8 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  summon_captain_planet.map {|planet| planet.capitalize + "!"}
+  new_array.map {|planet| planet.capitalize + "!"}
+  new_array
 end
 
 def long_planeteer_calls# code an argument here
