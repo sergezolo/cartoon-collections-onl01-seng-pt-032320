734e2e90cc026779f7edc2ef0e873205f060c4f0
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-02 23:45:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,6 @@ def long_planeteer_calls(array)
   array.any? {|word| word.length > 4}
 end
 
-def find_the_chees(array)
+def find_the_cheese(array)
   array.include?("cheddar", "gouda", "camembert")
 end
