7764822f6aa11009734f6e2539ac15de1ee3d7b5
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-02 23:55:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|cheese| cheese_types.include?(cheese)
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+  end
 end
